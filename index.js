d4c94f8a055fc843af88a86c0e0a3e746c4e5721
yugmani <yugmani@yahoo.com>
2022-06-22 23:42:13 -0700

    use of 'call' function

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 // Import stylesheets
 import './style.css';
 
+// How to call a function with different contexts in JS
+// *******************************************************
+
+function Car(type, fuelType) {
+  this.type = type;
+  this.fuelType = fuelType;
+}
+
+function setBrand(brand) {
+  Car.call(this, 'convertible', 'gasoline');
+  this.brand = brand;
+  console.log(`Car details = `, this);
+}
+
+function definePrice(price) {
+  Car.call(this, 'convertible', 'diesel');
+  this.price = price;
+  console.log(`Car details = `, this);
+}
+
+const newBrand = new setBrand('Tesla');
+const newCarPrice = new definePrice('1million');
+
